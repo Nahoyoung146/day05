cf6a8258ef0ffebb62c1dd2a878a299c1539b908
나호영 <skghdud024@naver.com>
2024-10-03 10:55:57 +0900

    집에서수정, 로그인 문제 질문 있음

--- src/array_/Ex05.java
+++ src/array_/Ex05.java
@@ -2,8 +2,10 @@ package array_;
 
 public class Ex05 {
 	public static void main(String[] args) {
-		int[][][] arr = { {{ 1, 2, 3 }}, {{ 4, 5, 6} }, { {7, 8, 9} } };
-		System.out.println(arr[0][1][1]);
+		int[][][] arr = { {{ 1, 2, 3 },{10,11,12}}, {{ 4, 5, 6} }, { {7, 8, 9} } };
+		System.out.println( arr[0][1][1] );
+		System.out.println( arr[1][0][1] );
+		System.out.println( arr[2][0][1] );
 
 	}
 }
--- src/array_/Quiz1.java
+++ src/array_/Quiz1.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
 
 public class Quiz1 {
 	public static void main(String[] args) {
-//		int arr[] = new int[] { 10, 54, 13, 17, 25, 30 };
-//		for (int a : arr) {
-//			if (a % 2 == 0)
-//				System.out.println("짝수 : " + a);
-//			else
-//				System.out.println("홀수 : " + a);
-//		}
-
-//		Scanner input = new Scanner(System.in);
-//		int arr[] = new int[] { 10, 54, 13, 17, 25, 30 };
-//		String odd_even = null;
-//		System.out.print("짝수, 홀수 입력 : ");
-//		odd_even = input.next();
-//		if (odd_even.equals("짝수")) {
-//			for (int a : arr) {
-//				if (a % 2 == 0)
-//					System.out.println("짝수 : " + a);
-//			}
-//		} else {
-//			for (int a : arr) {
-//				if (a % 2 != 0)
-//					System.out.println("홀수 : " + a);
-//			}
-//		}
-
+		System.out.println("1. 배열의 짝수, 홀수 값을 따로 출력");
 		Scanner input = new Scanner(System.in);
 		int arr[] = new int[] { 10, 54, 13, 17, 25, 30 };
-		String odd_even = null;
+		for (int a : arr) {
+			if (a % 2 == 0)
+				System.out.println("짝수 : " + a);
+			else
+				System.out.println("홀수 : " + a);
+		}
+		System.out.println("====================================");
+
+		System.out.println("2. 짝수, 홀수를 입력받아 따로 출력");
+		System.out.print("짝수, 홀수 입력 : ");
+		String odd_even = input.next();
+		if (odd_even.equals("짝수")) {
+			for (int a : arr) {
+				if (a % 2 == 0)
+					System.out.println("짝수 : " + a);
+			}
+		} else {
+			for (int a : arr) {
+				if (a % 2 != 0)
+					System.out.println("홀수 : " + a);
+			}
+		}
+		System.out.println("====================================");
+
+		System.out.println("3. 짝수, 홀수를 입력받고 index가 짝수, 홀수인 값을 출력");
 		System.out.print("짝수, 홀수 입력 : ");
 		odd_even = input.next();
 		if (odd_even.equals("짝수")) {
@@ -44,6 +44,23 @@ public class Quiz1 {
 				if (i % 2 != 0)
 					System.out.println(arr[i]);
 			}
+		}
+
+		System.out.println("------다른 방법1------");
+		int k;
+		if (odd_even.equals("짝")) {
+			k = 0;
+		} else {
+			k = 1;
+		}
+		for (; k < arr.length; k += 2) {
+			System.out.println(arr[k]);
+		}
+
+		System.out.println("------다른 방법2------");
+		// 변수 = (식) ? 참 : 거짓;
+		for (k = odd_even.equals("짝") ? 0 : 1; k < arr.length; k += 2) {
+			System.out.println(arr[k]);
 		}
 	}
 }
